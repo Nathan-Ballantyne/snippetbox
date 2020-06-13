6f62ae9b0398613af5d2037d5ccbfcbd7680aa7a
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-06-13 15:24:06 +1200

    refactor createSnippet function to use http.Error instead of w.WriteHeader and w.Write

--- main.go
+++ main.go
@@ -24,20 +24,14 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check whether the request is using POST or not. Note that
-	// http.MethodPost is a constant equal to the string "POST".
 	if r.Method != http.MethodPost {
-		// Use the Header().Set() method to add an 'Allow: POST' header to the
-		// response header map. The first parameter is the header name, and
-		// the second parameter is the header value.
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
 		return
-
 	}
-
-	w.Write([]byte("Create a new snippet"))
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
