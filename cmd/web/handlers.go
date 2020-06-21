972246c0b2a8bb9d9732e908ae09875f7cda0390
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-06-21 16:02:55 +1200

    update showSnippet method to use html tmpl files to display

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	//"html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -67,6 +67,28 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 			app.serverError(w, err)
 		}
 		return
+	}
+
+	// Initialize a slice containing the paths to the show.page.tmpl file,
+	// plus the base layout and footer partial that we made earlier.
+	files := []string{
+		"./ui/html/show.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	// Parse the template files...
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// And then execute them. Notice how we are passing in the snippet
+	// data (a models.Snippet struct) as the final parameter.
+	err = ts.Execute(w, s)
+	if err != nil {
+		app.serverError(w, err)
 	}
 
 	fmt.Fprintf(w, "%v", s)
