f70ddef365ac78d354ea604183801a045beff904
Nathan <nathan.ballantyne@gmail.com>
2020-06-24 19:40:04 +1200

    change snippet url patterm matching

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,10 +11,8 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+	// Because Pat matches the "/" path exactly, we can now remove the manual check
+	// of r.URL.Path != "/" from this handler.
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -27,12 +25,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-// Change the signature of the showSnippet handler so it is defined as a method
-// against *application.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Pat doesn't strip the colon from the named capture key, so we need to
+	// get the value of ":id" from the query string instead of "id".
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
-		app.notFound(w) // Use the notFound() helper.
+		app.notFound(w)
 		return
 	}
 
@@ -57,26 +55,24 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 // Change the signature of the showSnippet handler so it is defined as a method
 // against *application.
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	// Checking if the request method is a POST is now superfluous and can be
+	// removed.
 
-	// Create some variables holding dummy data. I'll remove these later on
-	// during the build.
 	title := "0 snail"
 	content := "0 snail\nClimb Mount Fuji, \nBut slowly!\n\n- Kobayashi Issa"
 	expires := "7"
 
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	// Redirect the user to the relevant page for the snippet
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	// Change the redirect to use the new semantic URL style of /snippet/:id
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
+
+// Add a new createSnippetForm handler, which for now returns a placeholder response.
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
