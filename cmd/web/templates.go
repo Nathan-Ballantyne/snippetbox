56fb057c2c384ca40e32ca07042f856fa6514545
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-06-27 18:08:07 +1200

    add properties to templateData struct to hold\n partially submitted form data and form errors

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"net/url"
 	"path/filepath"
 	"time"
 
@@ -11,30 +12,23 @@ import (
 // Add a CurrentYear field to the templateData struct.
 type templateData struct {
 	CurrentYear int
+	FormData    url.Values
+	FormErrors  map[string]string
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
 
-// Create a humanDate function which returns a nicely formatted string
-// representation of a time.Time object.
 func humanDate(t time.Time) string {
 	return t.Format("02 Jan 2006 at 15:04")
 }
 
-// Initialize a template.FuncMap object and store it in a global variable. This is
-// essentially a string-keyed map which acts as a lookup between the names of our
-// custom template functions and the functions themselves.
 var functions = template.FuncMap{
 	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
 	cache := map[string]*template.Template{}
 
-	// Use the filepath.Glob function to get a slice of all filepaths with
-	// the extension '.page.tmpl'. This essentially gives us a slice of all the
-	// 'page' templates for the application.
 	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
 	if err != nil {
 		return nil, err
